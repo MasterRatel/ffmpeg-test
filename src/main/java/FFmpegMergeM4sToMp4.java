7d84c521082b6eaca322078368f7cd38b39db0ff
chenchen <chenchen3@37.com>
2024-07-15 12:19:01 +0800

    优化视频合并代码&添加日志输出

--- src/main/java/FFmpegMergeM4sToMp4.java
+++ src/main/java/FFmpegMergeM4sToMp4.java
@@ -12,49 +12,34 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-
 public class FFmpegMergeM4sToMp4 {
 
-	public static void main(String[] args) {
-//设置文件编码格式
-		System.setProperty("file.encoding", "UTF-8");
-//		设置需要遍历合并视频的目录
-		File rootDir = new File("D:/tmp/test/download");
-//		File rootDir = new File("D:/tmp/test/download");
+	//	开启日志，并使用追加模式记录信息
+	private static final Logger logger = setupLogger(true);
 
-//		开始进行目录下文件查找,从第一层开始找
+	// 定义一个正则表达式，匹配Windows文件名中不允许的字符
+	private static final String INVALID_CHARACTERS_REGEX = "[<>:\"/\\\\\\s\\.|?*]";
+
+
+	public static void main(String[] args) {
+		// 设置文件编码格式
+		System.setProperty("file.encoding", "UTF-8");
+		// 设置需要遍历合并视频的目录
+		File rootDir = new File("D:/tmp/test/download");
+		// 开始进行目录下文件查找,从第一层开始找
 		traverseDirectory(rootDir, 1);
+		System.out.println("------------->>>"+rootDir+"文件夹下的视频合并任务，全部运行完毕，记得检查日志噢<<<-------------");
 	}
 
-/*	public static void main(String[] args) {
-//设置文件编码格式
-		System.setProperty("file.encoding", "UTF-8");
-//		设置需要遍历合并视频的目录
-//		Scanner scanner = new Scanner(System.in);
-//
-//		System.out.print("需要遍历合并视频的目录: ");
-//		String listVideoDir = scanner.nextLine(); // 读取一行文本
-		File rootDir;
-//		if (!listVideoDir.isEmpty()){
-//			rootDir = new File(listVideoDir);
-//		}else {
-////			指定默认需要遍历合并视频的目录
-//			rootDir = new File("D:\\tmp\\test\\download");
-//		}
-		rootDir = new File("D:\\tmp\\test\\download");
-//		开始进行目录下文件查找,从第一层开始找
-		traverseDirectory(rootDir, 1);
-	}*/
-
 	private static void traverseDirectory(File dir, int level) {
-//		判断是否为第5层目录,且是否为有效目录
+		// 判断是否为第4层目录,且是否为有效目录
 		if (level == 4 && dir.isDirectory()) {
-//			对目录内m4s文件进行合并处理
+			// 对目录内m4s文件进行合并处理
 			mergeM4SFilesInDirectory(dir);
 			return;
 		}
 
-//递归遍历每一个子文件夹进行查找对应的第4层目录
+		// 递归遍历每一个子文件夹进行查找对应的第4层目录
 		File[] subDirs = dir.listFiles(File::isDirectory);
 		if (subDirs != null) {
 			for (File subDir : subDirs) {
@@ -64,30 +49,17 @@ public class FFmpegMergeM4sToMp4 {
 	}
 
 	private static void mergeM4SFilesInDirectory(File directory) {
-//		开启日志记录
-		Logger logs = loggerInfo(true);
 		File videoFile = new File(directory, "video.m4s");
 		File audioFile = new File(directory, "audio.m4s");
 		String entryJson = directory.getParent() + "\\entry.json";
+
 		// 解析 JSON 字符串到 JSONObject
 		Map<String, String> outProperties = getOutProperties(entryJson);
-		String title = outProperties.get("title").replaceAll("[\\\\\\|\\s\\.&</]", "-");
-		String part = outProperties.get("part").trim().replaceAll("[\\\\\\|\\s\\.&</]", "-").replaceAll("(?i)\\.mp4", "");
-//		System.out.println("------------------->>>开始进行视频合并<<<-------------------");
-//		System.out.println("视频文件路径: " + videoFile);
-//		System.out.println("音频文件路径: " + audioFile);
-//		System.out.println("配置文件路径: " + entryFile);
-//		System.out.println("获取到的标题是: " + outProperties.get("title"));
-//		System.out.println("获取到的文件名是: " + outProperties.get("part"));
+		String title = sanitizeFileName(outProperties.get("title"));
+		String part = sanitizeFileName(outProperties.get("part").replaceAll("(?i)\\.mp4", ""));
 
+		logger.info("视频合并的全路径目录是: " + directory);
 
-		System.out.println("视频合并的全路径目录是: " + directory.toString());
-		/*Scanner scanner = new Scanner(System.in);
-
-		System.out.print("合并视频输出的根目录: ");
-		String outputFileRootPath = scanner.nextLine(); // 读取一行文本
-		// 定义默认的输出文件目录和视频名称
-		String outputDir = !outputFileRootPath.isEmpty() ? outputFileRootPath : "D:\\tmp\\test\\download";*/
 		String outputDir = "D:\\tmp\\test";
 		String videoName = "output.mp4";
 		if (title != null && !title.isEmpty()) {
@@ -108,55 +80,56 @@ public class FFmpegMergeM4sToMp4 {
 		String outputFilePath = new File(outputDir, videoName).getAbsolutePath();
 
 		File videoFileTmp = new File(outputFilePath);
-//		控制台打印相关日志
-//		outputFilePath = Pattern.quote(outputFilePath);
-//		outputFilePath = outputFilePath.replaceAll("\\s+", "");
-//		System.out.println("输出目录路径: " + outputDir);
-//		System.out.println("输出文件名称: " + videoName);
-		System.out.println("最终输出文件是: " + outputFilePath);
+		logger.info("最终输出文件是: " + outputFilePath);
+
 		if (videoFile.exists() && audioFile.exists() && !videoFileTmp.exists()) {
-//组装合并视频的操作命令
-			String ffmpeg = "D: & cd D:\\personSoftware\\ffmpeg-6.1.1-full_build\\bin & ffmpeg -i " + videoFile + " -i " + audioFile + "  -codec copy " + outputFilePath;
+			// 组装合并视频的操作命令
+			String ffmpeg = "D: & cd D:\\personSoftware\\ffmpeg-6.1.1-full_build\\bin & ffmpeg -i " + videoFile + " -i " + audioFile + " -codec copy " + outputFilePath;
 			List<String> cmdList = new ArrayList<>();
 			cmdList.add("cmd");
 			cmdList.add("/c");
 			cmdList.add(ffmpeg);
-			try {
-				ProcessBuilder processBuilder = new ProcessBuilder(cmdList);
-				processBuilder.directory(directory);
-				processBuilder.redirectErrorStream(true);
-
-				Process process = processBuilder.start();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("UTF-8")));
-
-//				 ffmpag的日志输出,排查问题时开启
-				String line;
-				while ((line = reader.readLine()) != null) {
-					System.out.println(line);
-				}
-				int exitCode = process.waitFor();
-				if (exitCode == 0) {
-					System.out.println("最终执行的状态码（0为成功）是: " + exitCode);
-				} else {
-					logs.info("异常：合并过程出现异常。当前目录是:" + directory);
-//					System.out.println("异常：合并过程出现异常,最终执行的状态码（0为成功）: " + exitCode);
-				}
-			} catch (IOException | InterruptedException e) {
-				e.printStackTrace();
-			}
+			executeCommand(cmdList, directory);
 		} else if (videoFileTmp.exists()) {
-			logs.info("视频已合并,跳过执行: " + videoFileTmp);
-
-//			System.out.println("视频已合并,跳过执行: " + videoFileTmp.toString());
+			logger.info("视频已合并,跳过执行: " + videoFileTmp);
 		} else {
-			logs.info("异常：合并时发现video.m4s或audio.m4s不在目标路径: " + directory.getAbsolutePath());
-
-//			System.out.println("异常：合并时发现video.m4s或audio.m4s不在目标路径: " + directory.getAbsolutePath());
+			logger.info("异常：合并时发现video.m4s或audio.m4s不在目标路径: " + directory.getAbsolutePath());
 		}
 	}
 
-	public static Map<String, String> getOutProperties(String entryJson) {
+	private static void executeCommand(List<String> cmdList, File directory) {
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder(cmdList);
+			processBuilder.directory(directory);
+			processBuilder.redirectErrorStream(true);
 
+			Process process = processBuilder.start();
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("UTF-8")))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					logger.info(line);
+				}
+			}
+			int exitCode = process.waitFor();
+			if (exitCode == 0) {
+				logger.info("最终执行的状态码（0为成功）是: " + exitCode);
+			} else {
+				logger.severe("异常：合并过程出现异常。异常目录是:" + directory + "，状态码: " + exitCode);
+			}
+		} catch (IOException | InterruptedException e) {
+			logger.log(Level.SEVERE, "执行命令时发生异常", e);
+		}
+	}
+
+	private static String sanitizeFileName(String inputString) {
+		if (inputString == null || inputString.isEmpty()) {
+			return inputString;
+		}
+		// 使用正则表达式替换非法字符
+		return inputString.replaceAll(INVALID_CHARACTERS_REGEX, "-");
+	}
+
+	public static Map<String, String> getOutProperties(String entryJson) {
 		Map<String, String> outProperties = new HashMap<>();
 
 		// 读取 JSON 文件并进行解析
@@ -170,13 +143,12 @@ public class FFmpegMergeM4sToMp4 {
 			String jsonString = jsonContent.toString();
 			JSONObject jsonObject = JSON.parseObject(jsonString);
 
-
 			// 获取 标题 文件名
-//			String title = jsonObject.containsKey("title") ? jsonObject.getString("title"): "未知标题";
 			String title;
 			if (jsonObject.containsKey("title")) {
 				title = jsonObject.getString("title");
 			} else {
+				logger.info("异常：解析标题异常："+entryJson);
 				title = "未知标题";
 			}
 			outProperties.put("title", title);
@@ -189,18 +161,19 @@ public class FFmpegMergeM4sToMp4 {
 			} else if (jsonObject.containsKey("ep") && !jsonObject.getJSONObject("ep").getString("index").isEmpty()) {
 				part = jsonObject.getJSONObject("ep").getString("index");
 			} else {
+				logger.info("异常：解析文件名异常："+entryJson);
 				part = "未知文件名";
 			}
 			outProperties.put("part", part);
 
 			return outProperties;
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.log(Level.SEVERE, "读取 JSON 文件时发生异常", e);
 		}
 		return outProperties;
 	}
 
-	public static Logger loggerInfo(boolean appendLogs) {
+	private static Logger setupLogger(boolean appendLogs) {
 		try {
 			// 获取日志记录器
 			Logger logger = Logger.getLogger("LoggingToFile");
@@ -209,7 +182,7 @@ public class FFmpegMergeM4sToMp4 {
 			logger.setLevel(Level.ALL);
 
 			// 创建文件处理器
-			FileHandler fileHandler = new FileHandler("app.log", 10 * 1024 * 1024, 1, appendLogs);
+			FileHandler fileHandler = new FileHandler("app.log", 20 * 1024 * 1024, 1, appendLogs);
 
 			// 创建日志格式
 			SimpleFormatter formatter = new SimpleFormatter();
@@ -217,12 +190,10 @@ public class FFmpegMergeM4sToMp4 {
 
 			// 将文件处理器添加到日志记录器
 			logger.addHandler(fileHandler);
-			// 返回日志记录器
 			return logger;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		return null;
+		return Logger.getLogger("LoggingToFile");
 	}
 }
